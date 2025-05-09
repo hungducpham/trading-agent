305ea184b7ac477c92294f3aecf8a9b68efa20f1
Duc Hung Pham <hungdpham92@gmail.com>
2025-05-09 15:06:01 -0700

    small fix

--- trading_strategy.py
+++ trading_strategy.py
@@ -3,9 +3,5 @@ class TradingStrategy:
     Base class for the different trading strategies. The different strategies must implement the perform_action method.
     """
 
-    def __init__(self, df, symbols):
-        self.df = df
-        self.symbols = symbols
-
     def perform_action(self, date, data, portfolio, cash):
         pass
